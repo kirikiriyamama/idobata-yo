5ff42b886007b953181457c45a5e5bb0a8723de3
remain <ponde.ponde.ponde@gmail.com>
2014-07-29 00:39:10 +0900

    Create minimal application

---
+++ config.ru
@@ -0,0 +1,3 @@
+require_relative "idobata-yo"
+
+run Sinatra::Application
---
+++ idobata-yo.rb
@@ -0,0 +1,27 @@
+require "faraday"
+require "sinatra"
+
+configure :development do
+  require "dotenv"
+  Dotenv.load
+end
+
+configure do
+  set :faraday, Faraday.new(ENV["IDOBATA_HOOK_URL"]) { |faraday|
+    faraday.request :url_encoded
+    faraday.adapter Faraday.default_adapter
+  }
+end
+
+get "/:yo_callback_path" do
+  return 400 unless params[:username]
+  return 400 unless params[:yo_callback_path] == ENV["YO_CALLBACK_PATH"]
+
+  begin
+    settings.faraday.post(nil, source: "Yo from #{params[:username]}")
+  rescue Faraday::ClientError
+    halt 500
+  end
+
+  200
+end
